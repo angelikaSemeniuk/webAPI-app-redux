fd6db17cdbd1a456d9d7d5b9cbbe386384bcfacd
Anzhelika <angelika0094@gmail.com>
2018-10-18 14:53:40 +0300

    refactoring code

--- containers/PaginationContainer.js
+++ containers/PaginationContainer.js
@@ -12,11 +12,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         handlePreviousPage : (currentPage) => {
-            console.error("action-PAGE", currentPage);
             dispatch(handlePreviousPage(currentPage));
         },
         handleNextPage: (currentPage) => {
-            console.error("action-PAGE", currentPage);
             dispatch(handleNextPage(currentPage));
         }
     }
@@ -25,7 +23,11 @@ const mapDispatchToProps = (dispatch) => {
 const paginationContainer = (props) => {
     return (
         <div className="pagination">
-            <button className="previousPage" onClick={props.handlePreviousPage.bind(this, props.currentPage)}>Previous page</button>
+            {(props.currentPage !== 1) ? (
+                <button className="previousPage" onClick={props.handlePreviousPage.bind(this, props.currentPage)}>Previous page</button>
+            ) : (
+                <button className="previousPage" disabled>Previous Page</button>
+            )}
             <span className="currentPage" dangerouslySetInnerHTML={{__html: props.currentPage}}></span>
             <p className="numberOfPages" dangerouslySetInnerHTML={{__html: props.totalPages}}></p>
             <button className="nextPage" onClick={props.handleNextPage.bind(this, props.currentPage)}>Next page</button>
